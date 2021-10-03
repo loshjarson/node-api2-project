847f20af606e026a26d4124dfe0315036de9e974
unknown <jlarson129@outlook.com>
2021-10-03 17:28:26 -0400

    added PUT endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -49,4 +49,25 @@ router.post('/', (req,res) => {
     }
 })
 
+server.put("/api/users/:id",async (req,res) => {
+    const {id} = req.params
+    const changes = req.body
+    try{
+        if(!changes.title || !changes.contents){
+            res.status(400).json({ message: "Please provide title and contents for the post" })
+        }else{
+            const updatedPost = await Post.update(id,changes)
+            if(!updatedPost){
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }else{
+                res.status(200).json(updatedPost)
+            }
+        }    
+    }
+    catch(err){
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+})
+
+
   module.exports = router
