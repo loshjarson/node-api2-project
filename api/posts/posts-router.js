d7acf4942f298d155e2f6d3eed7ad5741f0db604
unknown <jlarson129@outlook.com>
2021-12-06 11:42:58 -0500

    get user by id and post new user

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -13,6 +13,40 @@ router.get('/', (req, res) => {
         console.log(error);
         res.status(500).json({ message: "The posts information could not be retrieved" });
       });
-  });
+});
 
-  
+router.get('/:id', (req,res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "The post information could not be retrieved" });
+          });
+})
+
+router.post('/', (req,res) => {
+    const newPost = req.body
+    if(!newPost.title || !newPost.contents){
+        res.status(400).json({ message: "Please provide title and contents for the post" })
+    }else{
+        Post.insert(newPost)
+        .then(postID=>{
+            Post.findById(postID.id)
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({ message: "There was an error while saving the post to the database" })
+            })            
+        })
+        .catch(err=>{
+            res.status(500).json({ message: "There was an error while saving the post to the database" })
+        })
+    }
+})  
