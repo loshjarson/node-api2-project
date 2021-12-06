53c832d69cc3716058bd372d8afe4a780275a354
unknown <jlarson129@outlook.com>
2021-12-06 11:46:28 -0500

    get user comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -101,3 +101,19 @@ router.delete('/:id', (req, res)=>{
           });
 })
 
+router.get('/:id/comments', (req, res) => {
+    Post.findPostComments(req.params.id)
+      .then(comments => {
+        if (comments.length > 0) {
+          res.status(200).json(comments);
+        } 
+        else {
+          res.status(404).json({ message: "The post with the specified ID does not exist" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: "The comments information could not be retrieved" });
+      });
+  })
+module.exports = router
